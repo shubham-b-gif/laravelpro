e6b6a41a054ffccc21f95df5b6969e2b7640b879
shubham <sbotfole@gmail.com>
2021-04-23 20:54:55 -0700

    migration done

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+//list of article
+Route::get('article','ArticleController@index');
+//list of single article
+Route::get('article/{id}','ArticleController@show');
+//create new article 
+Route::post('article','ArticleController@store');
+//update article
+Route::put('article','ArticleController@store');
+//delete article
+Route::delete('article','ArticleController@destroy');
+
+
